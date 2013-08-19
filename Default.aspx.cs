d636c3640e996690da0b82af758c8fec8d3b7faf
1773003 <1773003@mydbs.ie>
2013-08-19 14:26:31 +0100

    1st commit

---
+++ Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnEquals_Click(object sender, EventArgs e)
+    {
+        int num1;
+        int num2;
+        num1 = int.Parse(txtNo1.Text);
+        num2 = int.Parse(txtNo2.Text);
+        lblResult.Text = (num1+num2).ToString();
+    }
+}
